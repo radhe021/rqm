d62ac378acdd466b1b94f91feaf9ea8df0c4d67c
rxs <radheyiitr021@outlook.com>
2022-08-22 11:19:36 +0530

    Add codestyle checks

--- src/App.js
+++ src/App.js
@@ -1,56 +1,51 @@
 /** @format */
 
-import React, { useState, useEffect } from "react";
-import "./App.css";
-import axios from "axios";
-import QuoteBox from "./QuoteBox";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import axios from 'axios';
+import QuoteBox from './QuoteBox';
 
 function App() {
-	const [quotestatement, setquotestatement] = useState("");
-	const [fetchNewQuote, setfetchNewQuote] = useState(false);
-	var colors = [
-		"#16a085",
-		"#27ae60",
-		"#2c3e50",
-		"#f39c12",
-		"#e74c3c",
-		"#9b59b6",
-		"#FB6964",
-		"#342224",
-		"#472E32",
-		"#BDBB99",
-		"#77B1A9",
-		"#73A857",
-	];
-	const color = Math.floor(Math.random() * colors.length);
+  const [quotestatement, setquotestatement] = useState('');
+  const [fetchNewQuote, setfetchNewQuote] = useState(false);
+  const colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+  const color = Math.floor(Math.random() * colors.length);
 
-	useEffect(() => {
-		async function fetchData() {
-			const fetchData = await axios.get(
-				" https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json"
-			);
-			setquotestatement(
-				fetchData.data.quotes[
-					Math.floor(Math.random() * fetchData.data.quotes.length)
-				]
-			);
-		}
-		fetchData();
-	}, [fetchNewQuote]);
+  useEffect(() => {
+    async function fetchData() {
+      const fetchQuoteData = await axios.get(
+        ' https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json'
+      );
+      setquotestatement(
+        fetchQuoteData.data.quotes[Math.floor(Math.random() * fetchData.data.quotes.length)]
+      );
+    }
+    fetchData();
+  }, [fetchNewQuote]);
 
-	return (
-		<div
-			className="App card"
-			style={{ backgroundColor: `${colors[color]}`, color: 'whitesmoke' }}
-		>
-			<QuoteBox
-				quote={quotestatement}
-				setfetchNewQuote={setfetchNewQuote}
-				fetchNewQuote={fetchNewQuote}
-				color={`${colors[color]}`}
-			/>
-		</div>
-	);
+  return (
+    <div className="App card" style={{ backgroundColor: `${colors[color]}`, color: 'whitesmoke' }}>
+      <QuoteBox
+        color={`${colors[color]}`}
+        fetchNewQuote={fetchNewQuote}
+        quote={quotestatement}
+        setfetchNewQuote={setfetchNewQuote}
+      />
+    </div>
+  );
 }
 
 export default App;
